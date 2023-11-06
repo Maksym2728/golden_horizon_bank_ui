5849fc7bb8027a33d787ef8b2a3b6a112753eeaf
max <maksymkononenko28@gmail.com>
2023-11-06 20:15:03 +0200

    feature: GHB-1 / added header and footer

---
+++ src/app/home-page/home/questions-section/questions-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-questions-section',
+  templateUrl: './questions-section.component.html',
+  styleUrls: ['./questions-section.component.scss']
+})
+export class QuestionsSectionComponent {
+
+}
